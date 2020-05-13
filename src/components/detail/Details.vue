f1fa1f64307d26c7845ec56598081efbcffe71f6
zhangfengjin <16643208662@163.com>
2020-05-13 16:48:35 +0800

    静态列表页完成

--- src/components/detail/Details.vue
+++ src/components/detail/Details.vue
@@ -21,28 +21,56 @@
               工程实施
             </template>
             <ul class="tableul">
-              <li>
-                <div class="liNum">1</div>
+              <li v-for="(it,i) in nameArr" :key="i">
+                <div class="liNum">{{i+1}}</div>
                 <div class="licontent">
                   <div class="liConTop">
-                    <div class="liIndex">liIndex</div>
-                    <div class="liUnit">liUnit</div>
-                    <div class="liResult">liResult</div>
+                    <div
+                      class="liIndex"
+                    >人，这辈子想活得好，就一定得心态好。有些事，不斤斤计较，有些人，不必放在心上，心量大了，能承受的就多了，生活就不那么苦了。心态好，一切都好，心态差，万事皆难，人生下半场，拼的就是一种心态</div>
+                    <div class="liUnit">
+                      <ul>
+                        <li>a.)我一直相信一句话：当你感到生活困难时，你就是在走上坡路，你就是在成长。</li>
+                        <li>b.)我一直相信一句话：当你感到生活困难时，你就是在走上坡路，你就是在成长。</li>
+                        <li>c.)我一直相信一句话：当你感到生活困难时，你就是在走上坡路，你就是在成长。</li>
+                        <li>c.)我一直相信一句话：当你感到生活困难时，你就是在走上坡路，你就是在成长。</li>
+                        <li>c.)我一直相信一句话：当你感到生活困难时，你就是在走上坡路，你就是在成长。</li>
+                        <li style="line-height:25px;padding:0 10px;">
+                          <label>
+                            <input type="checkbox">
+                            <span>我一直相信一句话：当你感到生活困难时，你就是在走上坡路，你就是在成长。</span>
+                          </label>
+                        </li>
+                        <li style="line-height:25px;padding:0 10px;">
+                          <label>
+                            <input type="checkbox">
+                            <span>我一直相信一句话：当你感到生活困难时，你就是在走上坡路，你就是在成长。</span>
+                          </label>
+                        </li>
+                        <li style="line-height:25px;padding:0 10px;">
+                          <label>
+                            <input type="checkbox">
+                            <span>我一直相信一句话：当你感到生活困难时，你就是在走上坡路，你就是在成长。</span>
+                          </label>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="liResult">
+                      <el-select v-model="value" placeholder="请选择">
+                        <el-option
+                          v-for="item in options"
+                          :key="item.value"
+                          :label="item.label"
+                          :value="item.value"
+                        ></el-option>
+                      </el-select>
+                    </div>
                     <div class="liFile">liFile</div>
                   </div>
-                  <div class="liConBottom">备注</div>
-                </div>
-              </li>
-              <li>
-                <div class="liNum">2</div>
-                <div class="licontent">
-                  <div class="liConTop">
-                    <div class="liIndex">liIndex</div>
-                    <div class="liUnit">liUnit</div>
-                    <div class="liResult">liResult</div>
-                    <div class="liFile">liFile</div>
+                  <div class="liConBottom">
+                    <span style="float:left;display:block">备注:</span>
+                    <el-input v-model="input" placeholder="请输入内容"></el-input>
                   </div>
-                  <div class="liConBottom">备注</div>
                 </div>
               </li>
             </ul>
@@ -60,12 +88,56 @@ export default {
       activeName: "0",
       // nameArr:['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19']
       nameArr: ["1", "2", "3", "4"],
+      options: [
+        {
+          value: "选项1",
+          label: "黄金糕",
+        },
+        {
+          value: "选项2",
+          label: "双皮奶",
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎",
+        },
+        {
+          value: "选项4",
+          label: "龙须面",
+        },
+        {
+          value: "选项5",
+          label: "北京烤鸭",
+        },
+      ],
+      value: "",
+      input: "",
     };
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      // 遍历显示大小写字母
+      for (var i = 0; i < 26; i++) {
+        console.log(String.fromCharCode(65 + i)); //输出A-Z 26个大写字母
+      }
+      for (var i = 0; i < 26; i++) {
+        console.log(String.fromCharCode(97 + i)); //输出a-z 26个小写字母
+      }
+    },
   },
 };
 </script>
 
 <style>
+.detail {
+  font-size: 14px;
+  letter-spacing: 0px;
+  text-align: justify;
+  user-select: none;
+}
 .liConTop {
   width: 100%;
   height: 80%;
@@ -77,8 +149,20 @@ export default {
 .liConBottom {
   width: 100%;
   height: 20%;
+  line-height: 30px;
   position: relative;
   float: left;
+  padding: 5px;
+  box-sizing: border-box;
+}
+.liConBottom .el-input {
+  width: 90%;
+}
+.liConBottom .el-input__inner {
+  width: 100%;
+  height: 30px;
+  float: left;
+  border: none;
 }
 .liNum {
   width: 5%;
@@ -96,27 +180,30 @@ export default {
   height: 100%;
   position: relative;
   float: left;
+  line-height: 20px;
 }
 .liIndex {
-  width: 25%;
+  width: 26%;
   height: 100%;
   position: relative;
   float: left;
   padding: 5px;
   border-right: 1px solid #ccc;
   box-sizing: border-box;
+  overflow: auto;
 }
 .liUnit {
-  width: 50%;
+  width: 53%;
   height: 100%;
   position: relative;
   padding: 5px;
   float: left;
   border-right: 1px solid #ccc;
   box-sizing: border-box;
+  overflow: auto;
 }
 .liResult {
-  width: 10%;
+  width: 10.5%;
   height: 100%;
   position: relative;
   padding: 5px;
@@ -124,14 +211,57 @@ export default {
   border-right: 1px solid #ccc;
   box-sizing: border-box;
 }
+.liResult .el-input__inner {
+  height: 40px;
+  border-radius: 0;
+}
+.liResult .el-select .el-input.is-focus .el-input__inner {
+  border-color: #c0c4cc;
+}
+.liResult .el-select .el-input__inner:focus {
+  border-color: #c0c4cc;
+}
+/* .liResult  .el-input__suffix{
+  top: 5px;
+} */
+.el-popper[x-placement^="bottom"] {
+  margin-top: 5px !important;
+  border-radius: 0px;
+}
 .liFile {
-  width: 10%;
+  width: 10.5%;
   height: 100%;
   position: relative;
   box-sizing: border-box;
   padding: 5px;
   float: left;
+  overflow: auto;
 }
+.list::-webkit-scrollbar,
+.liIndex::-webkit-scrollbar,
+.liUnit::-webkit-scrollbar,
+.liFile::-webkit-scrollbar {
+  width: 3px;
+}
+.list::-webkit-scrollbar-thumb,
+.liIndex::-webkit-scrollbar-thumb,
+.liUnit::-webkit-scrollbar-thumb,
+.liFile::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+   -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+   background: rgba(0, 0, 0, 0.2);
+}
+.list::-webkit-scrollbar-track,
+.liIndex::-webkit-scrollbar-track,
+.liUnit::-webkit-scrollbar-thumb,
+.liFile::-webkit-scrollbar-thumb {
+  -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+   border-radius: 0;
+   background: rgba(0, 0, 0, 0.1);
+}
+/* ,.liUnit::-webkit-scrollbar,.liFile::-webkit-scrollbar{
+  width: 3px;
+} */
 .title {
   width: 100%;
   height: 40px;
